e17b179f0568ce00b1c8372f998296f32001687a
Roni Lindholm <roni.lindholm@gmail.com>
2025-11-03 08:56:35 +0200

    feat: customize default qfc url

--- src/core/qfieldcloud/qfieldcloudconnection.cpp
+++ src/core/qfieldcloud/qfieldcloudconnection.cpp
@@ -107,7 +107,7 @@ void QFieldCloudConnection::setUrl( const QString &url )
 
 QString QFieldCloudConnection::defaultUrl()
 {
-  return QStringLiteral( "https://app.qfield.cloud" );
+  return qfield::defaultCloudUrl;
 }
 
 QStringList QFieldCloudConnection::urls() const
